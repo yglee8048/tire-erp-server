54f3d56386e1e5af30d16c953967e646d85ad541
yglee8048 <yglee@lgcns.com>
2021-01-01 21:18:27 +0900

    first commit

---
+++ src/main/java/com/minsoo/co/tireerpserver/TireErpServerApplication.java
@@ -0,0 +1,13 @@
+package com.minsoo.co.tireerpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TireErpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TireErpServerApplication.class, args);
+	}
+
+}
