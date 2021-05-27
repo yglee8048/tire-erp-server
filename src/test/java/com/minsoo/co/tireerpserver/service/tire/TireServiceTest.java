48c0f7c56b644a5f02f565afd408ebc718348e3b
yglee <yglee@lgcns.com>
2021-05-28 00:33:26 +0900

    # 타이어 기본 테스트 작성

--- src/test/java/com/minsoo/co/tireerpserver/service/tire/TireServiceTest.java
+++ src/test/java/com/minsoo/co/tireerpserver/service/tire/TireServiceTest.java
@@ -1,6 +1,8 @@
 package com.minsoo.co.tireerpserver.service.tire;
 
 import com.minsoo.co.tireerpserver.api.error.exceptions.AlreadyExistException;
+import com.minsoo.co.tireerpserver.api.error.exceptions.NotFoundException;
+import com.minsoo.co.tireerpserver.model.dto.tire.TireRequest;
 import com.minsoo.co.tireerpserver.model.entity.entities.management.Brand;
 import com.minsoo.co.tireerpserver.model.entity.entities.management.Pattern;
 import com.minsoo.co.tireerpserver.model.entity.entities.tire.Tire;
@@ -26,43 +28,66 @@ class TireServiceTest extends ServiceTest {
     TireService tireService;
 
     /**
-     * 기본적인 타이어 CU 테스트 (브랜드 생성 포함)
-     * 타이어 상품 ID가 중복되는 경우, 예외가 발생해야 한다.
+     * 타이어 ID가 중복되는 경우, 예외가 발생해야 한다.
      */
     @Test
     @DisplayName("타이어 생성")
     void create() {
+        // GIVEN
         Brand brand = brandService.create(BRAND("테스트 브랜드"));
         Pattern pattern = patternService.create(brand.getId(), PATTERN("테스트 패턴"));
+        TireRequest tireRequest = TIRE("PRODUCT_ID_01", pattern.getId(), 11);
+        TireRequest duplicateRequest = TIRE("PRODUCT_ID_01", pattern.getId(), 11);
         clear();
 
-        log.debug("타이어 생성 테스트");
+        // WHEN
+        Tire tire = tireService.create(tireRequest);
+        clear();
+
+        // THEN
+        assertThat(tire.getProductId()).isEqualTo("PRODUCT_ID_01");
+        assertThatThrownBy(() -> tireService.create(duplicateRequest))
+                .isInstanceOf(AlreadyExistException.class);
+        clear();
+    }
+
+    @Test
+    @DisplayName("타이어 수정")
+    void update() {
+        // GIVEN
+        Brand brand = brandService.create(BRAND("테스트 브랜드"));
+        Pattern pattern = patternService.create(brand.getId(), PATTERN("테스트 패턴"));
         Tire tire = tireService.create(TIRE("PRODUCT_ID_01", pattern.getId(), 11));
         clear();
 
-        Tire found = tireService.findById(tire.getId());
-        assertThat(found.getProductId()).isEqualTo("PRODUCT_ID_01");
-
-        log.debug("타이어 상품 ID 중복 생성 테스트");
-        assertThatThrownBy(() -> tireService.create(TIRE("PRODUCT_ID_01", pattern.getId(), 11)))
-                .isInstanceOf(AlreadyExistException.class);
+        // WHEN
+        Tire updated = tireService.update(tire.getId(), TIRE("PRODUCT_ID_02", pattern.getId(), 22));
         clear();
 
-        log.debug("타이어 수정 테스트");
-        Tire updated = tireService.update(tire.getId(), TIRE("PRODUCT_ID_02", pattern.getId(), 22));
-
+        // THEN
         assertThat(updated.getProductId()).isEqualTo("PRODUCT_ID_02");
-
-        assertThat(tire.getSize()).isNotEqualTo(updated.getSize());
+        assertThat(updated.getInch()).isEqualTo(22);
         assertThat(updated.getSize().endsWith("22")).isTrue();
+        clear();
     }
 
-    /**
-     * TODO: 타이어 삭제 테스트
-     */
     @Test
-    @DisplayName("타이어 삭제 테스트")
+    @DisplayName("타이어 삭제")
     void removeTireTest() {
+        // GIVEN
+        Brand brand = brandService.create(BRAND("테스트 브랜드"));
+        Pattern pattern = patternService.create(brand.getId(), PATTERN("테스트 패턴"));
+        Tire tire = tireService.create(TIRE("PRODUCT_ID_01", pattern.getId(), 11));
+        clear();
 
+        // WHEN
+        tireService.removeById(tire.getId());
+        clear();
+
+        // THEN
+        assertThat(tireService.findAll().size()).isEqualTo(0);
+        assertThatThrownBy(() -> tireService.findById(tire.getId()))
+                .isInstanceOf(NotFoundException.class);
+        clear();
     }
 }
