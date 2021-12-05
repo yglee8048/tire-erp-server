d53710b6c7420f91f9fc5405bc2b2c4ec6a2152f
yglee8048 <yglee8048@gmail.com>
2021-12-05 18:40:12 +0900

    # fix :sale

--- src/main/java/com/minsoo/co/tireerpserver/entity/sale/Sale.java
+++ src/main/java/com/minsoo/co/tireerpserver/entity/sale/Sale.java
@@ -5,7 +5,11 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Getter
@@ -13,5 +17,10 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "sale")
 public class Sale extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sale_id")
+    private Long id;
 
 }
