e238f2ff3239f3e81b105e920d62011fbe992490
yglee8048 <yglee8048@gmail.com>
2021-12-18 16:20:16 +0900

    # DTO swagger schema 적용

--- src/main/java/com/minsoo/co/tireerpserver/model/AddressDTO.java
+++ src/main/java/com/minsoo/co/tireerpserver/model/AddressDTO.java
@@ -1,6 +1,7 @@
 package com.minsoo.co.tireerpserver.model;
 
 import com.minsoo.co.tireerpserver.entity.Address;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,9 +9,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AddressDTO {
 
+    @Schema(name = "city", description = "시/도")
     private String city;
+    @Schema(name = "street_address", description = "도로명 주소")
     private String streetAddress;
+    @Schema(name = "detail_address", description = "세부 주소")
     private String detailAddress;
+    @Schema(name = "zip_code", description = "우편번호")
     private Integer zipCode;
 
     public AddressDTO(Address address) {
--- src/main/java/com/minsoo/co/tireerpserver/model/BusinessInfoDTO.java
+++ src/main/java/com/minsoo/co/tireerpserver/model/BusinessInfoDTO.java
@@ -1,6 +1,7 @@
 package com.minsoo.co.tireerpserver.model;
 
 import com.minsoo.co.tireerpserver.entity.BusinessInfo;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,15 +9,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BusinessInfoDTO {
 
+    @Schema(name = "business_number", description = "사업자 등록번호")
     private String businessNumber;
+    @Schema(name = "business_name", description = "이름")
     private String businessName;
+    @Schema(name = "business_type", description = "유형")
     private String businessType;
+    @Schema(name = "address", description = "주소")
     private AddressDTO address;
+    @Schema(name = "fax", description = "팩스번호")
     private String fax;
+    @Schema(name = "email", description = "이메일 주소")
     private String email;
+    @Schema(name = "representative", description = "대표")
     private String representative;
+    @Schema(name = "representativePhoneNumber", description = "대표 번호")
     private String representativePhoneNumber;
+    @Schema(name = "manager", description = "담당자")
     private String manager;
+    @Schema(name = "managerPhoneNumber", description = "담당자 번호")
     private String managerPhoneNumber;
 
     public BusinessInfoDTO(BusinessInfo businessInfo) {
